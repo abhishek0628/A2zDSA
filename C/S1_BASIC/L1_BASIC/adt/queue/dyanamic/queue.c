408096afb7b2539c5cf3abe278a3f93e82c547d3
Abhishek0628 <abhijnv124@gmail.com>
2026-04-27 23:45:40 +0530

    modified

--- C/S1_BASIC/L1_BASIC/adt/queue/dyanamic/queue.c
+++ C/S1_BASIC/L1_BASIC/adt/queue/dyanamic/queue.c
@@ -1,1 +1,15 @@
+//using linkedlist
 #include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+struct queue{
+    struct node *rear;
+    struct node *front;
+
+};
+int main(){
+    
+}
